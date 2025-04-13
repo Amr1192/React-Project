cc071f901f45494816927ac868cf44771041c1b5
Aya Hamdy <ayah39813@gmail.com>
2025-04-13 22:22:32 +0200

    second commit Aya Hamdy

--- project-react/src/store/store.js
+++ project-react/src/store/store.js
@@ -1,9 +1,12 @@
 // React Redux Toolkit RTK Query Sni  ==> (srts )
 
 import { configureStore } from "@reduxjs/toolkit";
+import wishCounter from "./slices/wishListCounter";
 
 const reduxStore = configureStore({
-  reducer: {},
+  reducer: {
+    wishList: wishCounter,
+  },
 });
 
 export default reduxStore;
